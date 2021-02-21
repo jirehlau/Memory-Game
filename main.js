4fda1254ab7069aa9b961dd1a4a72bca99c9083e
Jireh Lau <jirehcylau@gmail.com>
2021-02-20 23:51:53 -0500

    CSS and JS upate

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 //1. CONSTANT
-Score
-gridBox
-gameCell
-Timer
-
-
-
+card1 = "a";
+card2 = "a";
+card3 = "b";
+card4 = "b";
+card5 = "c";
+card6 = "c";
+card7 = "d";
+card8 = "d";
+card9 = "e";
+card10 = "e";
+card11 = "f";
+card12 = "f";
 
 //2. VARIABLES
-// let memCards = ["a","a","b","b","c","c","d","d","e","e","f","f"]
+Let score = 0;
+numOfCards = 12;
+userSelection1 = card1
+userSelection2 = card2
+userSelection3 = card3
+userSelection4 = card4
+userSelection5 = card5
+userSelection6 = card6
+userSelection7 = card7
+userSelection8 = card8
+userSelection9 = card9
+userSelection10 = card10
+userSelection11 = card11
+userSelection12 = card12
 
-let gameScore = 0
-let timerClock = 1000 * 60 // 1000 = 1 second in JS
 
 //3. ELEMENT REFERENCES
 
@@ -35,11 +51,7 @@ let timerClock = 1000 * 60 // 1000 = 1 second in JS
 
 
 
-// pseudo code
 
-// Game Grid: We are going to define the variables using an array and list out all the possible options that are going to be in the game
-
-// variables: score, reset button, time count, card flip
 
 // we are going to add an event listener click, so each time you click a card, it will flip over
 
@@ -53,31 +65,43 @@ let timerClock = 1000 * 60 // 1000 = 1 second in JS
 
 
 
+// CONSTANT
+
+// card1 = "a";
+// card2 = "a";
+// card3 = "b";
+// card4 = "b";
+
+// Define required variables used to track the state of the game
+
+// Let score = 0;
+// numOfCards = 12;
+// userSelection1 = card1
+// userSelection2 = card2
+// countDownTime = 1:00
 
 
-1) Define required constants
-const card1 = "a";
-const card2 = "b";
-const card3 = "b";
-const card4 ="a";
-
-// 2) Define required variables used to track the state of the game
-// Score
-// Time
-// state to track the correct pairs (How is this information going to go to the computer? Square 1 is A and square 3 is A...etc)
-// use 12 variables (card1, card2) and then card1 could represent the letter A
-computer must also know what the user has currently got correct
-
-3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
 
 
-4) Upon loading the app should:
-	4.1) Initialize the state variables
-	4.2) Render those values to the page
-	4.3) Wait for the user to click a square
+//FUNCTIONS
 
+// checkMatch ()
+// 1. check if the 2 cards matches
+// 	a. if the 2 cards match, it will stay opened
+// 	b. if the 2 cards do not match, it will flip back
 
-5) Handle a player clicking a square
+// flip()
+	// everytime a card is clicked, it will flip
+	// maximum number of 2 flips per turn 
+	
+// timer()
+// 1. Once you click on the first card, the timer will begin to coutndown
+// 2. Once the timer reaches "0" an alert will pop up saying "time is up" 
+// 	a. alert()
+// 3. if you beat the game before the timer is done, the time will stop. 
 
+// reset()
+// 1. Reset the game whether you are finished or not. once it resets timer will go back to 1 minute and all cards will be flipped back
 
-6) Handle a player clicking the replay button
+// score()
+// every time a pair of cards is matched, it will add 1 point
